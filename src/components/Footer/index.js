44a2e39d5d074a60a4044b01e88380637f9903bf
nordivi <victorw.nordi@icloud.com>
2023-11-22 18:18:29 -0300

    ✨ Copyright

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -103,6 +103,9 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
           
         </SocialMediaIcons>
+        <Copyright>
+          &copy; 2023 Rishav Chanda. All rights reserved.
+        </Copyright>
 
       </FooterWrapper>
     </FooterContainer>
